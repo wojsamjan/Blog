4bfe1183d7a4f22f53d9ab1856fcf16d549ec1b9
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-03 23:28:35 +0100

    update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # get 'home/index'
 
   root 'home#index'
-  get 'o-stronie' => 'home#about'
-  get 'kontakt' => 'home#contact'
+  get 'o-blogu' => 'home#about', as: :about
+  get 'kontakt' => 'home#contact', as: :contact
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
